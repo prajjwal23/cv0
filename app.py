c1f5e0454295aa4ba87827b071b8aeaed3d206ff
prajjwal23 <prajjwal.sonaje@mitaoe.ac.in>
2024-12-11 23:24:37 +0530

    third commit

--- app.py
+++ app.py
@@ -37,4 +37,7 @@ def process():
     return send_file(output_path, mimetype='image/jpeg')
 
 if __name__ == "__main__":
+    # Bind to the port provided by Render or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
